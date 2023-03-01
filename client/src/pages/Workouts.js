0e07ba06d0eda15f027360b5df9e91eef9ef77ca
palminski <williambolls98@gmail.com>
2023-03-01 14:44:15 -0800

    can select from your wokrouts on the workout page

--- client/src/pages/Workouts.js
+++ client/src/pages/Workouts.js
@@ -1,26 +1,41 @@
 import { useQuery } from "@apollo/client";
-import {QUERY_CURRENT_USER} from '../utils/queries'
+import {QUERY_CURRENT_USER} from '../utils/queries';
+import {useState} from 'react';
 
 const Workouts = () => {
     const {loading,data} = useQuery(QUERY_CURRENT_USER);
-    console.log(data);
+    
     const user = (data?.currentUser)
+    console.log(user.workouts);
 
     return (
         <>
-        <h1>Workouts</h1>
-        {
-            loading ? 
-            <>
-            <h2>Loading...</h2>
-            </>
-            :
-            <>
-            <h2>{user.username}</h2>
-            <h3>{user.workouts[0].name}</h3>
-            
-            </>
-        }
+            <h1>Workouts</h1>
+            {
+                loading ?
+                    <>
+                        <h2>Loading...</h2>
+                    </>
+                    :
+                    <>
+                        
+                        <h2>{user.username}</h2>
+                        {user.workouts && 
+                        <>
+                        <h2>{user.workouts.length}</h2>
+                        
+
+                        <label htmlFor="workouts">Select a Workout: </label>
+                        <select name="workouts">
+                            {user.workouts && user.workouts.map(workout => (
+                                <option key={workout._id} value={workout.name}>{workout.name}</option>
+                            ))}
+                        </select>
+                        </>
+                        }
+
+                    </>
+            }
         </>
     );
 }
